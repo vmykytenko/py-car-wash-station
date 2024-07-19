bf97c8033f1d3e37eeb18ba1a17d8abd4e528381
Vitalii Mykytenko <vitalii.mykytenko@gmail.com>
2024-07-19 21:53:30 +0300

    class CarWashStation: create method calculate_washing_price()

--- app/main.py
+++ app/main.py
@@ -20,3 +20,12 @@ class CarWashStation:
         self.average_rating = round(average_rating, 1)
         self.count_of_ratings = count_of_ratings
 
+   def calculate_washing_price(self, car: Car) -> float:
+        cost_for_a_single_car = round(
+            (
+                car.comfort_class * (self.clean_power - car.clean_mark)
+                * self.average_rating / self.distance_from_city_center
+            ), 1
+        )
+        return cost_for_a_single_car
+
